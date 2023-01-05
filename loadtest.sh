feb765e33e5e59254a79022131e657ca827facb4
Kyle Huntsman <3432646+kylehuntsman@users.noreply.github.com>
2023-01-05 14:03:48 -0700

    fix: add mkdir command to fix directory permissions

--- loadtest.sh
+++ loadtest.sh
@@ -2,13 +2,15 @@
 
 TEST_CONCURRENCIES=(1 8 16 32 64)
 
+mkdir -p out
+
 docker compose up -d influxdb grafana
 for CONCURRENCY in "${TEST_CONCURRENCIES[@]}"
 do
    if [[ -z "${RAW_LOAD_TEST}" ]]; then
-      UID=${UID} GID=${GID} SIMULTANEOUS_DOWNLOADS=$CONCURRENCY docker compose run k6 run --verbose /scripts/script.js
+      SIMULTANEOUS_DOWNLOADS=$CONCURRENCY docker compose run k6 run --verbose /scripts/script.js
    else
       source .env
-      UID=${UID} GID=${GID} K6_OUT=influxdb=http://127.0.0.1:8086/k6 BOOST_FETCH_URL=${BOOST_FETCH_URL} RAW_FETCH_URL=${RAW_FETCH_URL} SIMULTANEOUS_DOWNLOADS=$CONCURRENCY k6 run --verbose ./scripts/script.js
+      K6_OUT=influxdb=http://127.0.0.1:8086/k6 BOOST_FETCH_URL=${BOOST_FETCH_URL} RAW_FETCH_URL=${RAW_FETCH_URL} SIMULTANEOUS_DOWNLOADS=$CONCURRENCY k6 run --verbose ./scripts/script.js
    fi
 done
