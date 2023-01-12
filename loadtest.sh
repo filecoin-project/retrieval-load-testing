ae2e58a905c4744a4d979813d55b1927fb2ff2a6
Kyle Huntsman <3432646+kylehuntsman@users.noreply.github.com>
2023-01-11 20:34:53 -0700

    feat: add boost and raw fetch data received and bandwidth
    Changed RAW_LOAD_TEST to USE_DOCKER_K6. Removed --verbose in favor of --http-debug. Updated how the /out directory is defined.

--- loadtest.sh
+++ loadtest.sh
@@ -7,10 +7,10 @@ mkdir -p out
 docker compose up -d influxdb grafana
 for CONCURRENCY in "${TEST_CONCURRENCIES[@]}"
 do
-   if [[ -z "${RAW_LOAD_TEST}" ]]; then
-      SIMULTANEOUS_DOWNLOADS=$CONCURRENCY docker compose run k6 run --verbose /scripts/script.js
-   else
+   if [[ -z "${USE_DOCKER_K6}" ]]; then
       source .env
-      K6_OUT=influxdb=http://127.0.0.1:8086/k6 BOOST_FETCH_URL=${BOOST_FETCH_URL} RAW_FETCH_URL=${RAW_FETCH_URL} SIMULTANEOUS_DOWNLOADS=$CONCURRENCY k6 run --verbose ./scripts/script.js
+      K6_OUT=influxdb=http://127.0.0.1:8086/k6 BOOST_FETCH_URL=${BOOST_FETCH_URL} RAW_FETCH_URL=${RAW_FETCH_URL} SIMULTANEOUS_DOWNLOADS=$CONCURRENCY OUT_DIR="./out" k6 run --http-debug ./scripts/script.js
+   else
+      SIMULTANEOUS_DOWNLOADS=$CONCURRENCY OUT_DIR="/out" docker compose run k6 run --http-debug /scripts/script.js
    fi
 done
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 import http from 'k6/http'
 import { SharedArray } from 'k6/data'
-import encoding from 'k6/encoding'
-import { Trend, Rate } from 'k6/metrics'
+import { Trend, Rate, Counter } from 'k6/metrics'
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.3/index.js'
 import dayjs from 'https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js'
 
 const pieces = new SharedArray('pieces', function () {
@@ -13,14 +13,18 @@ const pieces = new SharedArray('pieces', function () {
   return arr // f must be an array[]
 })
 
-const timeBoost = new Trend('time-boost')
-const ttfbBoost = new Trend('ttfb-boost')
-const timeRaw = new Trend('time-raw')
-const ttfbRaw = new Trend('ttfb-raw')
-const timeDelta = new Trend('time-delta')
-const ttfbDelta = new Trend('ttfb-delta')
-const boostSuccess = new Rate('success-boost')
-const rawSuccess = new Rate('success-raw')
+const bandwidthBoost = new Trend('bandwidth_boost')
+const bandwidthRaw = new Trend('bandwidth_raw')
+const dataReceivedBoost = new Counter('data_received_boost')
+const dataReceivedRaw = new Counter('data_received_raw')
+const timeBoost = new Trend('time_boost')
+const timeDelta = new Trend('time_delta')
+const timeRaw = new Trend('time_raw')
+const ttfbBoost = new Trend('ttfb_boost')
+const ttfbDelta = new Trend('ttfb_delta')
+const ttfbRaw = new Trend('ttfb_raw')
+const boostSuccess = new Rate('success_boost')
+const rawSuccess = new Rate('success_raw')
 
 export const options = {
   scenarios: {
@@ -37,48 +41,70 @@ export const options = {
 export default function () {
   // get a random piece from the list
   const piece = pieces[Math.floor(Math.random() * pieces.length)]
-  // run raw vs boost randomly first
-  const runRawFirst = Math.round(Math.random())
+
+  // randomly fetch first from either a raw url or boost
+  const fetchRawUrlFirst = Math.random() >= .5
   let boostResponse, rawResponse
-  for (let i = 0; i < 2; i++) {
-    if ((i + runRawFirst) % 2 === 0) {
-      boostResponse = http.get(`${__ENV.BOOST_FETCH_URL}${piece}`, {
-        tags: {
-          name: 'BoostFetchURL',
-        },
-        timeout: `${__ENV.SIMULTANEOUS_DOWNLOADS}h`,
-      })
-      timeBoost.add(boostResponse.timings.duration)
-      ttfbBoost.add(boostResponse.timings.waiting)
-      boostSuccess.add(
-        boostResponse.status >= 200 && boostResponse.status < 300
-      )
-    } else {
-      if (__ENV.RAW_FETCH_URL) {
-        rawResponse = http.get(`${__ENV.RAW_FETCH_URL}${piece}`, {
-          tags: {
-            name: 'RawFetchURL',
-          },
-          timeout: `${__ENV.SIMULTANEOUS_DOWNLOADS}h`,
-        })
-        timeRaw.add(rawResponse.timings.duration)
-        ttfbRaw.add(rawResponse.timings.waiting)
-        rawSuccess.add(rawResponse.status >= 200 && rawResponse.status < 300)
-      }
-    }
+
+  if (fetchRawUrlFirst) {
+    rawResponse = fetchFromRawUrl(piece)
+    boostResponse = fetchFromBoost(piece)
+  } else {
+    boostResponse = fetchFromBoost(piece)
+    rawResponse = fetchFromRawUrl(piece)
   }
+
   if (__ENV.RAW_FETCH_URL) {
     timeDelta.add(boostResponse.timings.duration - rawResponse.timings.duration)
     ttfbDelta.add(boostResponse.timings.waiting - rawResponse.timings.waiting)
   }
 }
 
-const OUT_DIR = "/out";
+function fetchFromBoost(piece) {
+  let response = http.get(`${__ENV.BOOST_FETCH_URL}${piece}`, {
+    tags: {
+      name: 'BoostFetchURL',
+    },
+    timeout: `${__ENV.SIMULTANEOUS_DOWNLOADS}h`,
+  })
+  timeBoost.add(response.timings.duration)
+  ttfbBoost.add(response.timings.waiting)
+  boostSuccess.add(
+    response.status >= 200 && response.status < 300
+  )
+
+  let contentLength = parseInt(response.headers['Content-Length'])
+  dataReceivedBoost.add(contentLength, { url: response.url })
+  bandwidthBoost.add(contentLength / response.timings.duration)
+
+  return response
+}
+
+function fetchFromRawUrl(piece) {
+  if (__ENV.RAW_FETCH_URL) {
+    let response = http.get(`${__ENV.RAW_FETCH_URL}${piece}`, {
+      tags: {
+        name: 'RawFetchURL',
+      },
+      timeout: `${__ENV.SIMULTANEOUS_DOWNLOADS}h`,
+    })
+    timeRaw.add(response.timings.duration)
+    ttfbRaw.add(response.timings.waiting)
+    rawSuccess.add(response.status >= 200 && response.status < 300)
+
+    let contentLength = parseInt(response.headers['Content-Length'])
+    dataReceivedRaw.add(contentLength, { url: response.url })
+    bandwidthRaw.add(contentLength / response.timings.duration)
+
+    return response
+  }
+}
+
 const TEST_NAME = "script";
 
 export function handleSummary(data) {
   const timeStr = dayjs().format("YYYY-MM-DDTHH:mm:ss")
-  const filepath = `/${OUT_DIR}/${TEST_NAME}-${timeStr}.json`;
+  const filepath = `${__ENV.OUT_DIR}/${TEST_NAME}-${timeStr}.json`;
 
   return {
     'stdout': textSummary(data, { indent: "  ", enableColors: true }),
