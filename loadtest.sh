bfa8a9e9fc02e67cf3eba3074a88b6bfa9f1c3f5
Rod Vagg <rod@vagg.org>
2023-01-14 09:37:48 +1100

    fix: make env var naming consistent

--- loadtest.sh
+++ loadtest.sh
@@ -6,7 +6,7 @@
 ##   * USE_DOCKER_K6=1 to use the dockerized k6 instead of the local one
 ##   * SKIP_FULL_FETCH=1 to skip the full fetch tests
 ##   * SKIP_RANGE_REQUESTS=1 to skip the range requests tests
-##   * TEST_FETCH_CONCURRENCIES='X,Y,Z' to override the default concurrency levels for the full fetch tests
+##   * TEST_FULL_FETCH_CONCURRENCIES='X,Y,Z' to override the default concurrency levels for the full fetch tests
 ##   * TEST_RANGE_CONCURRENCIES='X,Y,Z' to override the default concurrency levels for the range requests tests
 ##   * TEST_RANGE_SIZES=='X,Y,Z' to override the default range sizes for the range requests tests
 ##   * SKIP_CSV_OUT=1 to skip the CSV output generation
@@ -27,16 +27,16 @@ function run_full_fetch() {
   TEST_NAME="full-fetch"
   mkdir -p out/"${TEST_NAME}"
 
-  if [ -z "$TEST_FETCH_CONCURRENCIES" ]; then
-    TEST_FETCH_CONCURRENCIES=(${DEFAULT_FULL_FETCH_CONCURRENCIES[@]})
+  if [ -z "$TEST_FULL_FETCH_CONCURRENCIES" ]; then
+    TEST_FULL_FETCH_CONCURRENCIES=(${DEFAULT_FULL_FETCH_CONCURRENCIES[@]})
   else
     # turn provided comma-separated list into an array
-    TEST_FETCH_CONCURRENCIES=(`echo $TEST_FETCH_CONCURRENCIES | tr ',' ' '`)
+    TEST_FULL_FETCH_CONCURRENCIES=(`echo $TEST_FULL_FETCH_CONCURRENCIES | tr ',' ' '`)
   fi
 
-  echo "Running full fetch with concurrencies: ${TEST_FETCH_CONCURRENCIES[@]}"
+  echo "Running full fetch with concurrencies: ${TEST_FULL_FETCH_CONCURRENCIES[@]}"
 
-  for CONCURRENCY in "${TEST_FETCH_CONCURRENCIES[@]}"; do
+  for CONCURRENCY in "${TEST_FULL_FETCH_CONCURRENCIES[@]}"; do
     if [[ -z "${USE_DOCKER_K6}" ]]; then
       source .env
       K6_OUT=influxdb=http://127.0.0.1:8086/k6 \
